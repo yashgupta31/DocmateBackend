70a098043159fda4115c272811bb013ac98ae470
yashgupta31 <yashsntoshgupta2019@gmail.com>
2024-12-13 00:08:07 +0530

    done some image work

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ dotenv.config()
 const connection= require('./config/db')
 const userRouter= require('./routes/user.route')
 const cors= require('cors')
+const path = require('path');
 const doctorRouter = require('./routes/doctor.route')
 const appointmentRouter = require('./routes/appointment.route')
 
@@ -16,7 +17,8 @@ app.use('/user', userRouter)
 app.use('/doctor', doctorRouter)
 app.use('/appointment', appointmentRouter);
 // Serve the "uploads" directory as static
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req, res)=>{
     res.send('home')
